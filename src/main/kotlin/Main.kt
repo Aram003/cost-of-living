193d72236bfea4ab4e168ba0aea2ac5bf5d877bc
Aram003 <aramabubakrce@gmail.com>
2024-06-05 02:27:21 +0300

    updated-main-called GetLowestFruitPrice.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,20 +3,27 @@ import dataSource.utils.CsvParser
 import interactor.CostOfLivingDataSource
 import interactor.GetCityHasCheapestInternetConnectionInteractor
 import interactor.GetHighestSalaryAverageCititesNamesInteractor
+import interactor.GetLowestFruitPrice
 
 fun main() {
     val csvParser = CsvParser()
     val dataSource: CostOfLivingDataSource = CsvDataSource(csvParser)
 
     val getHighestSalaryAverageCities = GetHighestSalaryAverageCititesNamesInteractor(dataSource)
+    println("Cities with highest average salary:")
     println(getHighestSalaryAverageCities.execute(limit = 10))
     printSeparationLine()
 
     val getCityHasCheapestInternetConnectionInteractor = GetCityHasCheapestInternetConnectionInteractor(dataSource)
+    println("City with cheapest internet connection:")
     println(getCityHasCheapestInternetConnectionInteractor.execute())
+    printSeparationLine()
 
-}
-private fun printSeparationLine(){
-    print("\n_______________________________\n")
+    val getLowestFruitPriceInteractor = GetLowestFruitPrice(dataSource)
+    println("Cities with lowest fruit prices:")
+    println(getLowestFruitPriceInteractor.getCitiesWithLowestFruitPrices(limit = 10))
 }
 
+private fun printSeparationLine() {
+    println("\n_______________________________\n")
+}
